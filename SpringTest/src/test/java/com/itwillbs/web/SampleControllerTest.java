806f1fc301b1a70034b13ca01d862b4dc95e13fa
inuit57@naver.com <inuit57@naver.com>
2021-06-24 12:21:07 +0900

    servlet 버전 변경
    테스트를 위해서 3.1.0 버전으로 변경

---
+++ SpringTest/src/test/java/com/itwillbs/web/SampleControllerTest.java
@@ -0,0 +1,11 @@
+package com.itwillbs.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SampleControllerTest {
+	private static final Logger logger = 
+			LoggerFactory.getLogger(SampleControllerTest.class);
+	
+	
+}
