96b89daf64d9571205b92dc4b22e499a479314de
ITWILL <ITWILL@DESKTOP-875I1EB>
2021-06-24 10:30:55 +0900

    파라미터 넘기는 방법 학습

--- SpringTest/src/main/java/com/itwillbs/web/SampleController2.java
+++ SpringTest/src/main/java/com/itwillbs/web/SampleController2.java
@@ -3,6 +3,7 @@ package com.itwillbs.web;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -22,4 +23,32 @@ public class SampleController2 {
 		
 		return "doB"; 
 	}
+	// String 타입으로 리턴하는 동작
+	// 주소와 다른 이름의 페이지를 보여줘야 하는 경우 String으로 리턴 
+
+//	@RequestMapping("/doB2")
+//	public String doB2() {
+//		logger.info("doB2() 메서드 호출 ");
+//		logger.info("String 타입 리턴 -> 리턴 결과.jsp 페이지 호출!"); 
+//		
+//		//return "doB"; 
+//		return "redirect:doB1"; 
+//	}
+	
+	// http://localhost:8088/web/doB2?msg=1000&txt=1234
+	@RequestMapping(value="/doB2" )
+	public String doB3(@ModelAttribute("msg") String msg ,
+						@ModelAttribute("txt") int txt ) {
+		
+		//msg = "test"; (이렇게는 사용 불가)
+		
+		logger.info("doB2() 메서드 호출"); 
+		logger.info("result.jsp 페이지로 이동"); 
+		logger.info("msg 정보 : " + msg);
+		logger.info("txt 정보 : " + txt); 
+		
+		return "result"; 
+	}
+
+	
 }
---
+++ SpringTest/src/main/webapp/WEB-INF/views/result.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<h1>result.jsp</h1>
+	
+	<h2>결과 : ${msg +1} </h2>
+	<h2>텍스트 : ${txt +1}</h2>
+</body>
+</html>
