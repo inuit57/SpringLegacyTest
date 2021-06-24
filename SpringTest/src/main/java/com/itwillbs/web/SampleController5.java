daa5d9c0fbd66ae5fcb0b16086e693dc78dc8d68
inuit57@naver.com <inuit57@naver.com>
2021-06-24 12:16:04 +0900

    json 데이터 생성용 dependency 추가

--- SpringTest/src/main/java/com/itwillbs/web/SampleController5.java
+++ SpringTest/src/main/java/com/itwillbs/web/SampleController5.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.itwillbs.domain.ProductVO;
 
@@ -14,12 +15,27 @@ public class SampleController5 {
 			LoggerFactory.getLogger(SampleController5.class); 
 	
 	@RequestMapping("/doPro")
-	public ProductVO doPRo() {
+	public ProductVO doPro() {
 		logger.info("doPro() 호출"); 
 		ProductVO vo = new ProductVO("휴대폰" , 200) ; 
 		
 		
 		return vo; 
 	}
+	// JSON 타입 데이터 활용 
+	
+	@RequestMapping("/doPro2")
+	public @ResponseBody ProductVO doPro2() {
+		logger.info("doPro() 호출"); 
+		ProductVO vo = new ProductVO("휴대폰" , 200) ; 
+		
+		return vo; 
+	}
+	//https://mvnrepository.com/artifact/com.fasterxml.jackson.core/jackson-databind/2.8.4
+	// pom.xml에 dependency 적용
+	// @ResponseBody 를 함수헤더에 적용 
+	// http://localhost:8088/web/doPro2 으로 확인해보면 json데이터로 주고 있는 것을 확인가능
+	
+	// 쉽게 json 데이터를 생성할 수 있는 방식. 
 	
 }
