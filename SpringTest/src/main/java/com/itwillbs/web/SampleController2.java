87d837c0b8796c525aa92408fbe973e3824257c3
ITWILL <ITWILL@DESKTOP-875I1EB>
2021-06-24 09:52:18 +0900

    requestmapping 학습 중

--- SpringTest/src/main/java/com/itwillbs/web/SampleController2.java
+++ SpringTest/src/main/java/com/itwillbs/web/SampleController2.java
@@ -12,6 +12,12 @@ public class SampleController2 {
 	@RequestMapping("/doB")
 	public String doB() {
 		logger.info("doB() 메서드 호출 ");
-		return ""; 
+		return "doA2"; 
+	}
+	
+	@RequestMapping("/doB1")
+	public String doB1() {
+		logger.info("doB1() 메서드 호출 ");
+		return "doB"; 
 	}
 }
