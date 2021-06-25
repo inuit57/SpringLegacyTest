d7c3e2848dd1375fc5565d271f5289344c547703
inuit57@naver.com <inuit57@naver.com>
2021-06-25 14:50:24 +0900

    mapper xml 파일 세팅

---
+++ SpringTest/src/main/java/com/itwillbs/domain/MemberVO.java
@@ -0,0 +1,56 @@
+package com.itwillbs.domain;
+
+import java.util.Date;
+
+public class MemberVO {
+	// tbl_member 테이블을 저장할 도메인 객체를 생성
+	
+	private String userid; 
+	private String userpw ; 
+	private String username ; 
+	private String useremail; 
+	private Date regdate ; 
+	private Date updateDate ;
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	public String getUserpw() {
+		return userpw;
+	}
+	public void setUserpw(String userpw) {
+		this.userpw = userpw;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getUseremail() {
+		return useremail;
+	}
+	public void setUseremail(String useremail) {
+		this.useremail = useremail;
+	}
+	public Date getRegdate() {
+		return regdate;
+	}
+	public void setRegdate(Date regdate) {
+		this.regdate = regdate;
+	}
+	public Date getUpdateDate() {
+		return updateDate;
+	}
+	public void setUpdateDate(Date updateDate) {
+		this.updateDate = updateDate;
+	}
+	@Override
+	public String toString() {
+		return "MemberVO [userid=" + userid + ", userpw=" + userpw + ", username=" + username + ", useremail="
+				+ useremail + ", regdate=" + regdate + ", updateDate=" + updateDate + "]";
+	} 
+	
+}
---
+++ SpringTest/src/main/java/com/itwillbs/persistence/MemberDAO.java
@@ -0,0 +1,9 @@
+package com.itwillbs.persistence;
+
+public interface MemberDAO {
+
+	// DB를 사용하는 기능 구현 ( 추상메서드 ) 
+	
+	// DB 서버의 시간 정보를 가져오는 기능
+	public String getTime(); 
+}
